a1a9c5b6287395b343173de8ee98629dc93e3fbe
leepataisia-pixel <leepataisia@gmail.com>
2025-10-27 13:26:00 +0100

    commit

---
+++ Assets/+++workdate/Scripts/Player/PlayerController.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    #region Insepctor Variables
+
+    [SerializeField]
+    private float walkingSpeed;
+    
+    #endregion
+
+    #region Private Variables
+
+    private InputSystem_Actions _inputActions;
+    private InputAction _moveAction;
+    private InputAction _jumpAction;
+    private InputAction _attackAction;
+    private InputAction _interactAction;
+    private InputAction _lookAction;
+    private InputAction _crouchAction;
+    private InputAction _previousAction;
+    private InputAction _nextAction;
+    private InputAction _sprintAction;
+    private InputAction _rollAction;
+    public Vector2 _moveInput;
+    #endregion
+
+    #region Unity Event Functions
+    /// <summary>
+    /// 
+    /// </summary>
+    private void Awake()
+    {
+        _inputActions = new InputSystem_Actions();
+        _moveAction = _inputActions.Player.Move;
+        _jumpAction = _inputActions.Player.Jump;
+        _attackAction = _inputActions.Player.Attack;
+        _interactAction = _inputActions.Player.Interact;
+        _lookAction = _inputActions.Player.Look;
+        _crouchAction = _inputActions.Player.Crouch;
+        _previousAction = _inputActions.Player.Previous;
+        _nextAction = _inputActions.Player.Next;
+        _sprintAction = _inputActions.Player.Sprint;
+        _rollAction = _inputActions.Player.Roll;
+       
+
+    }
+    private void OnEnable()
+    {
+        _inputActions.Enable();
+        _moveAction.performed += Move;
+        _moveAction.canceled += Move;
+
+    }
+    private void OnDisable()
+    {
+        _inputActions.Enable();
+        _moveAction.performed += Move;
+        _moveAction.canceled += Move;
+
+    }
+    #endregion 
+
+    #region Input 
+    private void Move(InputAction.CallbackContext ctx)
+    {
+        _moveInput = ctx.ReadValue<Vector2>();
+    }
+    #endregion
+}
