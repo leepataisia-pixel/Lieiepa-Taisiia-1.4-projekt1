b96db20a48fd239cb218e0d3fb83b09e66aa34be
leepataisia-pixel <leepataisia@gmail.com>
2025-11-23 22:12:49 +0100

    23.11

--- Assets/+++workdate/Scripts/Player/PlayerController.cs
+++ Assets/+++workdate/Scripts/Player/PlayerController.cs
@@ -5,18 +5,18 @@ namespace ___WorkData.Scripts.Player
 {
     [RequireComponent(typeof(Rigidbody2D))]
     [RequireComponent(typeof(Animator))]
-    [RequireComponent(typeof(SpriteRenderer))]
     public class PlayerController : MonoBehaviour
     {
-        // ИМЕНА ПАРАМЕТРОВ КАК В ТВОЁМ ANIMATOR
-        private static readonly int Hash_Movement      = Animator.StringToHash("Movement");
-        private static readonly int Hash_ActionID      = Animator.StringToHash("ActionID");
-        private static readonly int Hash_ActionTrigger = Animator.StringToHash("Action Trigger");
-        private static readonly int Hash_IsJumping     = Animator.StringToHash("isJumping");
-        private static readonly int Hash_OnGround      = Animator.StringToHash("onGround");
+        public static readonly int Hash_Movement      = Animator.StringToHash("Movement");
+        public static readonly int Hash_ActionID      = Animator.StringToHash("ActionID");
+        public static readonly int Hash_ActionTrigger = Animator.StringToHash("Action Trigger");
+
+        public static readonly int Hash_OnGround = Animator.StringToHash("onGround");
+        public static readonly int Hash_IsJumping = Animator.StringToHash("isJumping");
+        public static readonly int Hash_IsFalling = Animator.StringToHash("isFalling");
 
         [SerializeField] private float walkingSpeed = 5f;
-        [SerializeField] private float jumpSpeed    = 8f;
+        [SerializeField] private float jumpSpeed = 5f;
         [SerializeField] private Animator animator;
 
         private InputSystem_Actions _inputActions;
@@ -26,8 +26,11 @@ namespace ___WorkData.Scripts.Player
 
         private Vector2 _moveInput;
         private Rigidbody2D _rb;
-        private SpriteRenderer _spriteRenderer;
         private bool _lookingToTheRight = true;
+
+        private float clickDelay = 0.25f;
+        private float lastClickTime = -1f;
+        private int clickCount = 0;
 
         private void Awake()
         {
@@ -37,9 +40,8 @@ namespace ___WorkData.Scripts.Player
             _jumpAction   = _inputActions.Player.Jump;
             _attackAction = _inputActions.Player.Attack;
 
-            _rb             = GetComponent<Rigidbody2D>();
-            animator        = GetComponent<Animator>();
-            _spriteRenderer = GetComponent<SpriteRenderer>();
+            _rb = GetComponent<Rigidbody2D>();
+            animator = GetComponent<Animator>();
         }
 
         private void OnEnable()
@@ -50,70 +52,77 @@ namespace ___WorkData.Scripts.Player
             _moveAction.canceled  += Move;
 
             _jumpAction.performed   += OnJump;
-            _attackAction.performed += OnAttack;
+            _attackAction.performed += Attack;
         }
 
         private void OnDisable()
         {
             _moveAction.performed -= Move;
             _moveAction.canceled  -= Move;
-            _jumpAction.performed -= OnJump;
-            _attackAction.performed -= OnAttack;
+
+            _jumpAction.performed   -= OnJump;
+            _attackAction.performed -= Attack;
 
             _inputActions.Disable();
         }
 
         private void FixedUpdate()
         {
-            // Движение по X
             _rb.linearVelocity = new Vector2(_moveInput.x * walkingSpeed, _rb.linearVelocity.y);
 
-            // Параметр Movement для Move blend tree
             animator.SetFloat(Hash_Movement, Mathf.Abs(_rb.linearVelocity.x));
 
-            // Простая "земля": почти не двигаемся по Y → стоим на земле
             bool isGrounded = Mathf.Abs(_rb.linearVelocity.y) < 0.01f;
+            bool isJumping = _rb.linearVelocity.y > 0.1f;
+            bool isFalling = _rb.linearVelocity.y < -0.1f;
+
             animator.SetBool(Hash_OnGround, isGrounded);
-
-            // Прыгаем / падаем
-            bool isJumping = !isGrounded && _rb.linearVelocity.y > 0.01f;
             animator.SetBool(Hash_IsJumping, isJumping);
-
-            UpdateFlip();
+            animator.SetBool(Hash_IsFalling, isFalling);
         }
 
         private void Move(InputAction.CallbackContext ctx)
         {
             _moveInput = ctx.ReadValue<Vector2>();
-        }
 
-        private void UpdateFlip()
-        {
-            if (_moveInput.x > 0.01f)
+            if (_moveInput.x > 0f)
                 _lookingToTheRight = true;
-            else if (_moveInput.x < -0.01f)
+            else if (_moveInput.x < 0f)
                 _lookingToTheRight = false;
 
-            _spriteRenderer.flipX = !_lookingToTheRight;
+            UpdateRotation();
+        }
+
+        private void UpdateRotation()
+        {
+            transform.rotation = _lookingToTheRight
+                ? Quaternion.Euler(0, 0, 0)
+                : Quaternion.Euler(0, 180, 0);
         }
 
         private void OnJump(InputAction.CallbackContext ctx)
         {
             if (!ctx.performed) return;
 
-            // прыгаем только если почти на земле
-            bool isGrounded = Mathf.Abs(_rb.linearVelocity.y) < 0.01f;
-            if (!isGrounded) return;
-
             _rb.linearVelocity = new Vector2(_rb.linearVelocity.x, jumpSpeed);
         }
 
-        private void OnAttack(InputAction.CallbackContext ctx)
+        private void Attack(InputAction.CallbackContext ctx)
         {
             if (!ctx.performed) return;
 
-            // у тебя в переходах Attack стоит условие ActionID == 10
-            animator.SetFloat(Hash_ActionID, 10f);
+            float now = Time.time;
+
+            if (now - lastClickTime <= clickDelay)
+                clickCount++;
+            else
+                clickCount = 1;
+
+            lastClickTime = now;
+
+            int actionId = (clickCount == 1) ? 10 : 11;
+
+            animator.SetInteger(Hash_ActionID, actionId);
             animator.SetTrigger(Hash_ActionTrigger);
         }
     }
